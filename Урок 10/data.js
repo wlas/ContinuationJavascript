cd0fdc02ff9650b568ebfd70137b0c5de139577f
SandrLite <mr.wlas@mail.ru>
2023-09-22 18:51:55 +0300

    Task10

---
+++ Урок 10/data.js
@@ -0,0 +1,20 @@
+export const dataInfo = `[
+{
+    "id": "1",
+    "name": "Iliya",
+    "age": "35",
+    "url": "img/image1.jpg"
+},
+{
+    "id": "2",
+    "name": "Igor",
+    "age": "31",
+    "url": "img/image2.jpg"
+},
+{
+    "id": "3",
+    "name": "Alex",
+    "age": "55",
+    "url": "img/image3.jpg"
+}
+]`;
